1f7aadb87d4cd470c276df4d708dcce197f6773f
Evandro Carreira <evandro.carreira@gmail.com>
2014-04-22 12:18:05 -0300

    FertigoProRegular

--- include.php
+++ include.php
@@ -2,10 +2,21 @@
 <html lang="en">
     <head>
 		<title>Webfonts collection - just merge and include</title>
+
+		<link rel="stylesheet" href="/fonts/fertigopro_regular_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+
+		<style type="text/css">
+
+		.fertigopro	{ font-family: 'FertigoProRegular'; } 
+
+		</style>
+
     </head>
 
     <body>
 
+		<span class="fertigopro">FertigoProRegular teste</span>
+		
     </body>
 
 </html>
