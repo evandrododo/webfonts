f2795a8b95c5a41c79cac2669e6a75cb27ae78c0
Evandro Carreira <evandro.carreira@gmail.com>
2014-05-12 18:33:53 -0300

    Banda Regular Macroman

--- include.php
+++ include.php
@@ -2,6 +2,9 @@
 <html lang="en">
     <head>
 		<title>Webfonts collection - just merge and include</title>
+
+		<link rel="stylesheet" href="banda_regular_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+
     </head>
 
     <body>
