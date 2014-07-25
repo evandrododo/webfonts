bd5133256851785aef279fb75284a9bc9c509061
Evandro Carreira <evandro.carreira@gmail.com>
2014-07-25 08:39:19 -0300

    WC Mano Negra

--- include.php
+++ include.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-		<title>Webfonts collection - just merge and include</title>
+		<title>Webfonts collection - just merge and include</title>	
+
+		<link rel="stylesheet" href="css/fonts/wcmanonegrabta_regular_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+			
+		<style type="text/css">
+
+			.wc_mano_negra	{ font-family:"wc_mano_negra_btaregular",Georgia,serif;  }
+
+		</style>
+
+
     </head>
 
     <body>
+		
+		<span class="wc_mano_negra">WC Mano Negra teste</span><br>
 
     </body>
 
